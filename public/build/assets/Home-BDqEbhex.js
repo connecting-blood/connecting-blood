bfd65c572e342821a2220ce9f7f69e7c2cb94860
Ashif Sadiq <ashifsadiq@gmail.com>
2025-04-08 09:07:35 +0530

    publish Tue, Apr  8, 2025  8:54:15 AM

--- public/build/assets/Home-BDqEbhex.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-B7zgRo97.js";const r=()=>e.jsx("div",{children:"Home"});export{r as default};
--- public/build/assets/Users-CE1HaDl4.js
+++
@@ -1,1 +0,0 @@
-import{j as s,L as e}from"./app-B7zgRo97.js";function t(){return s.jsxs(s.Fragment,{children:[s.jsx(e,{title:"Users"}),s.jsx("h1",{children:"Users List"})]})}export{t as default};
